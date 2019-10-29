ea9a45854c35e86e82cfa32510a0121169c26178
vermakartik <eches1297@gmail.com>
2019-10-29 19:29:27 +0530

    build added

---
+++ precache-manifest.fc509c04b3c458cbd1358b6e32dc0886.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e147b3413ccaa7a9304e1dc38c52c995",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65639339e83789b4aac3",
+    "url": "/static/css/main.ffa3230d.chunk.css"
+  },
+  {
+    "revision": "9c4809ff44c2b399b371",
+    "url": "/static/js/0.23fce8b1.chunk.js"
+  },
+  {
+    "revision": "300fa116c318e5a6ddf6",
+    "url": "/static/js/1.8375380a.chunk.js"
+  },
+  {
+    "revision": "102be177370a3b6238b0",
+    "url": "/static/js/10.3ced9152.chunk.js"
+  },
+  {
+    "revision": "2b8044dea0a7a43bce0d",
+    "url": "/static/js/11.7d918e19.chunk.js"
+  },
+  {
+    "revision": "2e82397f837c8bef5bae",
+    "url": "/static/js/12.f8822d79.chunk.js"
+  },
+  {
+    "revision": "b925587f0cdf34298d86",
+    "url": "/static/js/13.6d968cad.chunk.js"
+  },
+  {
+    "revision": "ee544d3029262a8549d7",
+    "url": "/static/js/14.98edbb12.chunk.js"
+  },
+  {
+    "revision": "c540350725d9994355de",
+    "url": "/static/js/15.e8efcc8c.chunk.js"
+  },
+  {
+    "revision": "aacc88db0d2a9312e823",
+    "url": "/static/js/16.57ed9a42.chunk.js"
+  },
+  {
+    "revision": "7f5aff50531dcd198dd5",
+    "url": "/static/js/17.d4d2dabf.chunk.js"
+  },
+  {
+    "revision": "c0fd600c72e52606ac57",
+    "url": "/static/js/4.09ad899b.chunk.js"
+  },
+  {
+    "revision": "3e461abdc2ff826af87b",
+    "url": "/static/js/5.f0fcf8f2.chunk.js"
+  },
+  {
+    "revision": "d9767638fb54b5bd6c37",
+    "url": "/static/js/6.3436d7eb.chunk.js"
+  },
+  {
+    "revision": "945d7a08078ce9324ff3",
+    "url": "/static/js/7.6110f89c.chunk.js"
+  },
+  {
+    "revision": "70091ccee52bb4615c5a",
+    "url": "/static/js/8.69e442f0.chunk.js"
+  },
+  {
+    "revision": "b05d65ecde3855c9f9ac",
+    "url": "/static/js/9.d6f3327a.chunk.js"
+  },
+  {
+    "revision": "65639339e83789b4aac3",
+    "url": "/static/js/main.2b60d8c9.chunk.js"
+  },
+  {
+    "revision": "4857324e1e26a6f0f909",
+    "url": "/static/js/runtime-main.fe643d88.js"
+  },
+  {
+    "revision": "32e4bf76561ed5931f62eb2a87814324",
+    "url": "/static/media/IGMC.32e4bf76.jpg"
+  },
+  {
+    "revision": "e30c3f6f65eb0b2c8cbbc976be1c055b",
+    "url": "/static/media/igmc_menu.e30c3f6f.jpg"
+  }
+]);
